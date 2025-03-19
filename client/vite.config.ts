6880348503d744ab9e137129f96ac4db1519c898
Bob Parsons <deadstylebp@gmail.com>
2025-03-18 20:15:23 -0500

    update vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import Pages from 'vite-plugin-pages';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import Pages from "vite-plugin-pages";
 
 export default defineConfig({
   plugins: [
     Pages({
-      dirs: ['src/pages'],
+      dirs: ["src/pages"],
     }),
     solidPlugin(),
   ],
   server: {
-    port: 3000,
+    port: 33000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
